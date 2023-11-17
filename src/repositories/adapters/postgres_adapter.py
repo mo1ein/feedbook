255f96baac89ae251971c23b9a2922a1f6a48532
moein <moeinn.com@gmail.com>
2023-11-17 04:07:14 +0330

    feat: add feed adapters

--- src/repositories/adapters/postgres_adapter.py
+++ src/repositories/adapters/postgres_adapter.py
@@ -1,6 +1,7 @@
 from sqlalchemy import select
 
-from src.models.entities import UserEntity
+from src.models.entities import UserEntity, SourceEntity
+from src.models.feed_model import GetUserSourcesModel, SourceModel
 from src.models.user_model import UserModel
 from src.utils.orm.sqlalchemy_adapter import SqlAlchemyAdapter
 
@@ -19,3 +20,14 @@ class PostgresAdapter(SqlAlchemyAdapter):
         if (user_entity := self.execute(query).scalar()) is None:
             return None
         return UserModel.model_validate(user_entity)
+
+    def get_user_sources(self, input_model: UserModel) -> GetUserSourcesModel | None:
+        query = select(SourceEntity).where(SourceEntity.user_id == input_model.user_id)
+        if (source_entities := self.scalars(query).all()) is None:
+            return None
+        response_model = [SourceModel.model_validate(q) for q in source_entities]
+        return GetUserSourcesModel(user_sources=response_model)
+
+    def create_source(self, input_model: SourceModel) -> None:
+        source = SourceEntity(**input_model.model_dump(exclude_unset=True, exclude_none=True))
+        self.create(source)
