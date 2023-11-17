cc3d94c2451d8b30266ff355841a90aef27ac5fb
moein <moeinn.com@gmail.com>
2023-11-17 04:06:17 +0330

    fix: change user fields

--- src/models/user_model.py
+++ src/models/user_model.py
@@ -6,7 +6,8 @@ from pydantic import BaseModel, EmailStr, PastDatetime, ConfigDict, StrictStr
 class UserModel(BaseModel):
     model_config = ConfigDict(extra="forbid", validate_default=True, from_attributes=True, validate_assignment=True)
 
-    id: UUID | None = None
+    user_id: UUID | None = None
     email: EmailStr | None = None
     password: StrictStr | None = None
+    is_active: bool | None = None
     created_at: PastDatetime | None = None
