68d79f5cb8fd94155c19b1c3456145a6a6a73851
moein <moeinn.com@gmail.com>
2023-11-17 04:07:51 +0330

    feat: add source repository

--- src/repositories/repository.py
+++ src/repositories/repository.py
@@ -1,3 +1,4 @@
+from src.models.feed_model import GetUserSourcesModel, SourceModel
 from src.models.user_model import UserModel
 from src.repositories.adapters.postgres_adapter import PostgresAdapter
 
@@ -11,3 +12,9 @@ class Repository:
 
     def get_user(self, input_model: UserModel) -> UserModel:
         return self.postgres_adapter.get_user(input_model)
+
+    def get_user_sources(self, input_model: UserModel) -> GetUserSourcesModel:
+        return self.postgres_adapter.get_user_sources(input_model)
+
+    def add_user_source(self, input_model: SourceModel):
+        self.postgres_adapter.create_source(input_model)
