99559ef94ba3f6e06fa5865ab67e8680d9ba2d80
moein <moeinn.com@gmail.com>
2023-11-18 05:03:31 +0330

    fix: table relations

--- src/models/entities.py
+++ src/models/entities.py
@@ -15,27 +15,38 @@ class BaseEntity(DeclarativeBase):
 class FeedEntity(BaseEntity):
     __tablename__ = "feed"
     feed_id: Mapped[uuid.UUID] = Column(UUID(as_uuid=True), primary_key=True, server_default="DEFAULT")
+    user_id = Column(UUID, ForeignKey("users.user_id"), nullable=False)
     title = Column(String(), nullable=False)
     link = Column(String(), nullable=False)
     summary = Column(String(), nullable=False)
     author = Column(String(), nullable=False)
     published = Column(String(), nullable=False)
 
+    user = relationship("UserEntity", foreign_keys=[user_id])
+
+
 
 class SourceEntity(BaseEntity):
     __tablename__ = "source"
     source_id: Mapped[uuid.UUID] = Column(UUID(as_uuid=True), primary_key=True, server_default="DEFAULT")
-    user_id = Column(UUID, nullable=False)
+    user_id = Column(UUID, ForeignKey("users.user_id"), nullable=False)
     link = Column(String(), nullable=False)
+
+    user = relationship("UserEntity", foreign_keys=[user_id])
 
 
 class BookmarkEntity(BaseEntity):
     __tablename__ = "bookmark"
     bookmark_id: Mapped[uuid.UUID] = Column(UUID(as_uuid=True), primary_key=True, server_default="DEFAULT")
-    feed_id = Column(UUID, nullable=False)
-    user_id = Column(UUID, nullable=False)
+    feed_id = Column(UUID, ForeignKey("feed.feed_id"), nullable=False)
+    user_id = Column(UUID, ForeignKey("users.user_id"), nullable=False)
     created_at: Mapped[datetime] = Column(DateTime(), server_default="DEFAULT", nullable=False)
     updated_at: Mapped[datetime] = Column(DateTime(), nullable=False)
+
+    user = relationship("UserEntity", foreign_keys=[user_id])
+    feed = relationship("FeedEntity", foreign_keys=[feed_id])
+
+
 
 
 class UserEntity(BaseEntity):
@@ -44,4 +55,3 @@ class UserEntity(BaseEntity):
     email = Column(String(), unique=True, nullable=False)
     password = Column(String(), nullable=False)
     is_active = Column(Boolean(), nullable=False)
-
