f08e639cb3d15a461ebd9e6af61c9a2b1d50be1f
moein <moeinn.com@gmail.com>
2023-11-18 05:06:10 +0330

    feat: add feed repository

--- src/repositories/adapters/postgres_adapter.py
+++ src/repositories/adapters/postgres_adapter.py
@@ -1,7 +1,8 @@
-from sqlalchemy import select
+from sqlalchemy import select, and_
 
-from src.models.entities import UserEntity, SourceEntity
-from src.models.feed_model import GetUserSourcesModel, SourceModel
+from src.models.entities import UserEntity, SourceEntity, FeedEntity, BookmarkEntity
+from src.models.feed_model import GetUserSourcesModel, SourceModel, FeedModel, BookmarkModel, GetUserBookmarksModel, \
+    GetUserFeedsModel
 from src.models.user_model import UserModel
 from src.utils.orm.sqlalchemy_adapter import SqlAlchemyAdapter
 
@@ -15,19 +16,61 @@ class PostgresAdapter(SqlAlchemyAdapter):
         user_entity = self.create(user, return_data=True)
         return UserModel.model_validate(user_entity)
 
-    def get_user(self, input_model: UserModel) -> UserModel | None:
+    def get_user_by_email(self, input_model: UserModel) -> UserModel | None:
         query = select(UserEntity).where(UserEntity.email == input_model.email)
         if (user_entity := self.execute(query).scalar()) is None:
             return None
         return UserModel.model_validate(user_entity)
 
+    def get_user_by_id(self, input_model: UserModel | BookmarkModel) -> UserModel | None:
+        query = select(UserEntity).where(UserEntity.user_id == input_model.user_id)
+        if (user_entity := self.execute(query).scalar()) is None:
+            return None
+        return UserModel.model_validate(user_entity)
+
     def get_user_sources(self, input_model: UserModel) -> GetUserSourcesModel | None:
+        # TODO: fix none its handle with empty list
         query = select(SourceEntity).where(SourceEntity.user_id == input_model.user_id)
         if (source_entities := self.scalars(query).all()) is None:
             return None
         response_model = [SourceModel.model_validate(q) for q in source_entities]
         return GetUserSourcesModel(user_sources=response_model)
 
-    def create_source(self, input_model: SourceModel) -> None:
+    def create_source(self, input_model: SourceModel) -> SourceModel:
         source = SourceEntity(**input_model.model_dump(exclude_unset=True, exclude_none=True))
-        self.create(source)
+        source_entity = self.create(source, return_data=True)
+        return SourceModel.model_validate(source_entity)
+
+    def create_feed(self, input_model: FeedModel) -> FeedModel:
+        feed = FeedEntity(**input_model.model_dump(exclude_unset=True, exclude_none=True))
+        feed_entity = self.create(feed, return_data=True)
+        return FeedModel.model_validate(feed_entity)
+
+    def get_feed(self, input_model) -> FeedModel | None:
+        query = select(FeedEntity).where(
+            and_(
+                FeedEntity.feed_id == input_model.feed_id,
+                FeedEntity.user_id == input_model.user_id
+            )
+        )
+        if (feed_entity := self.execute(query).scalar()) is None:
+            return None
+        return FeedModel.model_validate(feed_entity)
+
+    def create_bookmark(self, input_model) -> BookmarkModel:
+        bookmark = BookmarkEntity(**input_model.model_dump(exclude_unset=True, exclude_none=True))
+        bookmark_entity = self.create(bookmark, return_data=True)
+        return BookmarkModel.model_validate(bookmark_entity)
+
+    def get_bookmark(self, input_model: BookmarkModel) -> BookmarkModel | None:
+        query = select(BookmarkEntity).where(BookmarkEntity.bookmark_id == input_model.bookmark_id)
+        if (bookmark_entity := self.execute(query).scalar()) is None:
+            return None
+        return BookmarkModel.model_validate(bookmark_entity)
+
+    def get_user_bookmarks(self, input_model: UserModel) -> GetUserFeedsModel:
+        query = select(FeedEntity).join(BookmarkEntity).where(
+            and_(FeedEntity.user_id == input_model.user_id, BookmarkEntity.user_id == input_model.user_id))
+        bookmark_entities = self.scalars(query).all()
+        response_model = [FeedModel.model_validate(e) for e in bookmark_entities]
+        return GetUserFeedsModel(user_feeds=response_model)
--- src/repositories/repository.py
+++ src/repositories/repository.py
@@ -1,4 +1,5 @@
-from src.models.feed_model import GetUserSourcesModel, SourceModel
+from src.models.feed_model import GetUserSourcesModel, SourceModel, FeedModel, BookmarkModel, GetUserBookmarksModel, \
+    GetUserFeedsModel
 from src.models.user_model import UserModel
 from src.repositories.adapters.postgres_adapter import PostgresAdapter
 
@@ -10,11 +11,32 @@ class Repository:
     def create_user(self, input_model: UserModel) -> UserModel:
         return self.postgres_adapter.create_user(input_model)
 
-    def get_user(self, input_model: UserModel) -> UserModel:
-        return self.postgres_adapter.get_user(input_model)
+    def get_user_by_email(self, input_model: UserModel) -> UserModel | None:
+        return self.postgres_adapter.get_user_by_email(input_model)
+
+    def get_user_by_id(self, input_model: UserModel | BookmarkModel) -> UserModel | None:
+        return self.postgres_adapter.get_user_by_id(input_model)
 
     def get_user_sources(self, input_model: UserModel) -> GetUserSourcesModel:
         return self.postgres_adapter.get_user_sources(input_model)
 
-    def add_user_source(self, input_model: SourceModel):
-        self.postgres_adapter.create_source(input_model)
+    def add_user_source(self, input_model: SourceModel) -> SourceModel:
+        return self.postgres_adapter.create_source(input_model)
+
+    def get_user_bookmarks(self, input_model: UserModel) -> GetUserFeedsModel:
+        return self.postgres_adapter.get_user_bookmarks(input_model)
+
+    def create_bookmark(self, input_model: BookmarkModel):
+        return self.postgres_adapter.create_bookmark(input_model)
+
+    def get_user_feeds(self, input_model: UserModel):
+        ...
+
+    def get_feed(self, input_model) -> FeedModel:
+        return self.postgres_adapter.get_feed(input_model)
+
+    def get_bookmark(self, input_model) -> BookmarkModel:
+        return self.postgres_adapter.get_bookmark(input_model)
+
+    def create_feed(self, input_model: FeedModel) -> FeedModel:
+        return self.postgres_adapter.create_feed(input_model)
