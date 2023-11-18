302ff17f79c42725e222b17c81c240dc16427b0e
moein <moeinn.com@gmail.com>
2023-11-18 05:09:16 +0330

    refactor: api tags with enum

--- src/services/public_services.py
+++ src/services/public_services.py
@@ -2,17 +2,17 @@ from src.logics.auth import AuthLogic
 from src.models.auth_model import RegisterOutputModel, LoginOutputModel, RegisterInputModel, LoginInputModel
 from fastapi import status, APIRouter
 
+from src.models.service_enum import ServiceType
+
 public_routes = APIRouter()
 
 
 @public_routes.post(
     "/auth/register",
     response_model=RegisterOutputModel,
-    # fix this
-    tags=["AUTH_API"],
+    tags=[ServiceType.AUTH_API],
     status_code=status.HTTP_201_CREATED
 )
-# TODO: this should be async or not?
 def register(input_model: RegisterInputModel) -> RegisterOutputModel:
     """Register user and create JWT access and refresh tokens"""
     return AuthLogic().register(input_model)
@@ -21,8 +21,7 @@ def register(input_model: RegisterInputModel) -> RegisterOutputModel:
 @public_routes.post(
     "/auth/login",
     response_model=LoginOutputModel,
-    # fix this
-    tags=["AUTH_API"],
+    tags=[ServiceType.AUTH_API],
     status_code=status.HTTP_200_OK
 )
 def login(input_model: LoginInputModel) -> LoginOutputModel:
