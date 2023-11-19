747eca6bedccecc314db5d7baf074f84dd136ad0
moein <moeinn.com@gmail.com>
2023-11-19 23:07:01 +0330

    feat: add test data in init_data.sq

--- db/init_data.sql
+++ db/init_data.sql
@@ -36,3 +36,7 @@ CREATE TABLE bookmark
     created_at  TIMESTAMP(6) WITHOUT TIME ZONE  NOT NULL DEFAULT transaction_timestamp(),
     updated_at  TIMESTAMP(6) WITHOUT TIME ZONE
 );
+
+-- test data
+INSERT INTO public.source (source_id, user_id, link, created_at) VALUES ('9945b97d-facd-4968-a152-f5284e720685', '4d43e0a5-2bec-439e-bdf1-bfb1239b767a', 'https://waylonwalker.com/rss.xml', '2023-11-19 14:43:52.168588');
+INSERT INTO public.feed (feed_id, user_id, title, link, summary, author, published, created_at) VALUES ('09e68633-5866-478a-a973-ab21142fef85', '4d43e0a5-2bec-439e-bdf1-bfb1239b767a', 'Heroicons', 'https://waylonwalker.com//thoughts-161', 'Here heroicons is a really nice set of many of the basic icons that you will need for building nice ui This post was a thought by', 'Waylon Walker', '2023-11-14', '2023-11-19 18:45:31.068979');
