0579c04a34d46e0e00ac39bbf79153aae20eb65a
moein <moeinn.com@gmail.com>
2023-11-20 04:09:35 +0330

    feat: add tests

--- tests/auth_logics_test.py
+++ tests/auth_logics_test.py
@@ -1,9 +1,34 @@
+from fastapi import HTTPException
+
+from src.logics.auth import AuthLogic
+from src.models.auth_model import LoginInputModel, LoginOutputModel, RegisterOutputModel, RegisterInputModel
+
+auth_logic = AuthLogic()
+
+
 def test_login():
-    ...
+    # login correct way
+    login_model = LoginInputModel(email='x@x.com', password='1qaz@WSX')
+    output = auth_logic.login(login_model)
+    assert isinstance(output, LoginOutputModel)
+
+    # email or password is wrong
+    login_model = LoginInputModel(email='akbar@gmail.com', password='9090ak@oOk$')
+    try:
+        output = auth_logic.login(login_model)
+    except HTTPException:
+        assert True
 
 
 def test_register():
-    ...
+    register_model = RegisterInputModel(
+        email='azhdar@gmail.com',
+        name='azhdar',
+        last_name='digikalaeian',
+        password='1qaz@WSX'
+    )
+    output = auth_logic.register(register_model)
+    assert isinstance(output, RegisterOutputModel)
 
 
 def test_verify_access_token():
--- tests/feed_logics_test.py
+++ tests/feed_logics_test.py
@@ -1,31 +1,72 @@
 from src.logics.feed import FeedLogic
-from src.models.feed_model import GetUserSourcesModel, SourceModel
+from src.models.feed_model import GetUserSourcesModel, SourceModel, GetUserFeedsModel, FeedModel, BookmarkModel
 from src.models.user_model import UserModel
 
 feed_logic = FeedLogic()
 
 
 def test_get_user_sources():
-    user_model = UserModel(user_id='4d43e0a5-2bec-439e-bdf1-bfb1239b767a')
+    user_model = UserModel(user_id='680813d9-ecec-4a60-970d-d34af9e398a6')
     source_model = SourceModel(
-        source_id="9945b97d-facd-4968-a152-f5284e720685",
-        user_id="4d43e0a5-2bec-439e-bdf1-bfb1239b767a",
+        source_id="fc9c3efd-cf90-44c8-9b24-cf5e812be080",
+        user_id="680813d9-ecec-4a60-970d-d34af9e398a6",
         link="https://waylonwalker.com/rss.xml",
-        created_at="2023-11-19 14:43:52.168588"
+        created_at="2023-11-19 22:09:58.802881"
     )
     expected = GetUserSourcesModel(user_sources=[source_model])
     output = feed_logic.get_user_sources(user_model)
     assert expected == output
-    # TODO: write wrong test
 
 
 def test_get_user_feeds():
-    ...
+    user_model = UserModel(user_id='680813d9-ecec-4a60-970d-d34af9e398a6')
+    expected = GetUserFeedsModel(
+        user_feeds=[
+            FeedModel(
+                feed_id="0bced263-a068-4eb5-97cf-294c6284d20f",
+                user_id="680813d9-ecec-4a60-970d-d34af9e398a6",
+                title="Heroicons",
+                link="https://waylonwalker.com//thoughts-161",
+                summary="Here heroicons is a really nice set of many of the basic icons that you will need for building nice ui This post was a thought by",
+                author="Waylon Walker",
+                published="2023-11-14",
+                created_at="2023-11-19 22:30:38.248052"
+            ),
+            FeedModel(
+                feed_id="0a694362-4d55-4e37-94be-2b6c95507a9b",
+                user_id="680813d9-ecec-4a60-970d-d34af9e398a6",
+                title="Uptime Kuma",
+                link="https://waylonwalker.com//thoughts-160",
+                summary="Here Uptime kuma is a fantastic self hosted monitoring tool.  One docker run command and you are up and running.  Once you are in you have full control over che",
+                author="Waylon Walker",
+                published="2023-11-11",
+                created_at="2023-11-19 22:30:38.248052"
+            ),
+        ]
+    )
+    output = feed_logic.get_user_feeds(user_model, posts_num=2)
+    assert expected == output
 
 
 def test_bookmark_feed():
-    ...
+    bookmark_model = BookmarkModel(
+        user_id='680813d9-ecec-4a60-970d-d34af9e398a6',
+        feed_id='0a694362-4d55-4e37-94be-2b6c95507a9b'
+    )
+    expected_user_id = bookmark_model.user_id
+    expected_feed_id = bookmark_model.feed_id
+    output = feed_logic.bookmark_feed(bookmark_model)
+    assert expected_user_id == output.user_id
+    assert expected_feed_id == output.feed_id
 
 
 def test_add_user_source():
-    ...
+    source_model = SourceModel(
+        user_id="680813d9-ecec-4a60-970d-d34af9e398a6",
+        link="https://news.ycombinator.com/rss",
+    )
+    expected_link = source_model.link
+    expected_user_id = source_model.user_id
+    output = feed_logic.add_user_source(source_model)
+    assert expected_link == output.link
+    assert expected_user_id == output.user_id
