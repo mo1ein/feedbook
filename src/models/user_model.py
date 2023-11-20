49b02c8969f5a2eef99bb2df2d4768aa5ae175f3
moein <moeinn.com@gmail.com>
2023-11-20 04:15:05 +0330

    refactor: UserModel

--- src/models/user_model.py
+++ src/models/user_model.py
@@ -1,13 +1,15 @@
 from uuid import UUID
 
-from pydantic import EmailStr, PastDatetime, ConfigDict, StrictStr
+from pydantic import EmailStr, PastDatetime, StrictStr
 
 from src.models.base_model import BaseFeedBookModel
 
 
 class UserModel(BaseFeedBookModel):
     user_id: UUID | None = None
+    name: StrictStr | None = None
+    last_name: StrictStr | None = None
     email: EmailStr | None = None
     password: StrictStr | None = None
-    is_active: bool | None = None
+    is_active: bool = True
     created_at: PastDatetime | None = None
